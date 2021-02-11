cbd7a2eb79ce36efc9f84f989a32428ad8cec345
mayurgpt07 <mayurgpt07@gmail.com>
2021-02-10 18:04:46 -0800

    DataLoader working succesfully

--- load_train_data.py
+++ load_train_data.py
@@ -16,23 +16,37 @@ import time
 import os
 
 class CustomDataSet(Dataset):
-    def __init__(self, main_dir, transform = None):
+    def __init__(self, main_dir, dataFrame, transform = None):
         self.main_dir = main_dir
         self.transform = transform
+        self.dataframe = dataFrame
         all_imgs = os.listdir(main_dir)
-        if ('.DS_Store' or './train_altered/.DS_Store' or './DS_Store') in all_imgs:
+        if ('.DS_Store' or './train/.DS_Store' or './DS_Store') in all_imgs:
             all_imgs.remove('.DS_Store')
         self.total_imgs = natsorted(all_imgs)
 
     def __len__(self):
-        return(self.total_imgs)
+        return len(self.total_imgs)
 
     def __getitem__(self, idx):
         img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
+        image_id = str(self.total_imgs[idx]).split('.')[0]
+        if len(self.dataframe.loc[self.dataframe['image_id'] == image_id, 'label']) == 0:
+            label = -1
+        else:
+            label = self.dataframe.loc[self.dataframe['image_id'] == image_id, 'label']
+            label = list(label)[0]
+
         image = Image.open(img_loc)
         tensor_image = self.transform(image)
-        return tensor_image
+        # print(torch.tensor(label))
+        # print(type(tensor_image))
+        print(label)
+        return tensor_image, label
 
+def aggregate_labels(df):
+    aggregate_df = df.groupby(by=['image_id'])['label'].apply(lambda x: x.value_counts().index[0]).reset_index()
+    return aggregate_df
 
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
@@ -49,11 +63,18 @@ le.fit(train_data['source'])
 train_data['label'] = le.transform(train_data['source'])
 train_data = train_data.drop('source', axis = 1)
 
-onlyfiles = [f for f in listdir('./train_altered')]
-# onlyfiles = [f.split('.')[0] for f in onlyfiles]
+agg_train_data = aggregate_labels(train_data)
+
+onlyfiles = [f for f in listdir('./train')]
 print(len(onlyfiles))
 
-img_folder_path = './train_altered/'
-my_dataset = CustomDataSet(img_folder_path, transform=transform)
+img_folder_path = './train/'
+my_dataset = CustomDataSet(img_folder_path, agg_train_data,transform=transform)
 print(len(list(my_dataset)))
-train_loader = data.DataLoader(my_dataset , batch_size=32, shuffle=False, num_workers=4, drop_last=True)
+train_loader = data.DataLoader(my_dataset , batch_size=1, shuffle=False, num_workers=4, drop_last = True)
+
+# print(list(train_loader), type(train_loader))
+for image, label in train_loader:
+    print(image.shape, label)
+
+# image, label = next(iter(train_loader))
